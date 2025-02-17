f14599d99a81fac9f352fa40c4310c38a654084f
soongu <naong526@naver.com>
2025-02-17 10:37:13 +0900

    # 3. 서버에서 이벤트목록 가져오기

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
 
 const EventsPage = () => {
 
+  const [eventList, setEventList] = useState([]);
+
+  useEffect(() => { 
+
+    const fetchEvents = async () => { 
+      const response = await fetch(`http://localhost:9000/api/events`);
+      const responseData = await response.json();
+
+      setEventList(responseData);
+    };
+
+    fetchEvents();
+
+  }, []);
+
+
   return (
     <>
-        <h1>Events Page</h1>
-        <ul></ul>
+      <h1>Events Page</h1>
+      <ul>
+        {
+          eventList.map(ev => (
+            <li key={ev.id}>{ ev.title }</li>
+          ))
+        }
+      </ul>
+      
     </>
   );
 };
