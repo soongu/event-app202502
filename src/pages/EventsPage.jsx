1cf548e48fb617857b5c846b421373ff9d5d4a7e
soongu <naong526@naver.com>
2025-02-18 11:16:58 +0900

    # 8. eventsLoader 아웃소싱

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -29,3 +29,16 @@ const EventsPage = () => {
 };
 
 export default EventsPage;
+
+// loader를 아웃소싱
+// export const loader = async () => fetch(`http://localhost:9000/api/events`);
+
+
+export const loader = async () => {
+  const res = await fetch(`http://localhost:9000/api/events`);
+  // const jsonData = await res.json();
+
+  // loader가 리턴한 데이터는 라우팅된 페이지와
+  // 그 컴포넌트의 하위 컴포넌트에서 언제든 뽑아서 사용할 수 있음
+  return res; // loader는 fetch결과를 바로 리턴하는 경우 json추출작업을 자동수행
+};
--- src/routes/route-config.jsx
+++ src/routes/route-config.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 import ErrorPage from '../pages/ErrorPage';
 import HomePage from '../pages/HomePage';
-import EventsPage from '../pages/EventsPage';
+import EventsPage, { loader as eventsLoader } from '../pages/EventsPage';
 import RootLayout from '../layout/RootLayout';
 import EventDetailPage from '../pages/EventDetailPage';
 import EventLayout from '../layout/EventLayout';
+
 
 const router = createBrowserRouter([
   {
@@ -24,16 +25,7 @@ const router = createBrowserRouter([
             index: true,
             element: <EventsPage />,
             // loader함수는 이 페이지가 라우팅될 때 자동으로 트리거되는 함수
-            loader: async () => {
-              // console.log('event loader call!');
-              const res = await fetch(`http://localhost:9000/api/events`);
-              const jsonData = await res.json();
-
-              // console.log(jsonData);
-              // loader가 리턴한 데이터는 라우팅된 페이지와
-              // 그 컴포넌트의 하위 컴포넌트에서 언제든 뽑아서 사용할 수 있음
-              return jsonData; 
-            }
+            loader: eventsLoader,
           },
           {
             path: ':eventId',
