da9b88d44d0fa99197484d492e610dcb9d52efee
soongu <naong526@naver.com>
2025-02-20 12:28:34 +0900

    # 31. 입력 포커스 이동

--- src/components/auth/VerificationInput.jsx
+++ src/components/auth/VerificationInput.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import styles from './SignUpForm.module.scss';
 
 const VerificationInput = () => {
@@ -10,7 +10,29 @@ const VerificationInput = () => {
     inputsRef.current[index] = $input;
   };
 
+  // 포커스를 다음칸으로 이동하는 함수
+  const focusNextInput = (index) => { 
+    // 인덱스 검증 - 마지막 칸에서는 포커스 이동 금지
+    if (index < inputsRef.current.length) {
+      inputsRef.current[index].focus();
+    } else {
+      // 마지막 칸에서는 포커스 해제
+      inputsRef.current[index - 1].blur();
+    }
+  };
+
+  // 입력이벤트
+  const handleNumber = (e, index) => { 
+    
+
+    // 입력이 끝나면 다음 칸으로 포커스 이동
+    focusNextInput(index);
+  };
+
   // 초기 렌더링시 첫번째 input에 포커싱
+  useEffect(() => { 
+    inputsRef.current[0].focus();
+  }, []);
 
   return (
     <>
@@ -23,7 +45,7 @@ const VerificationInput = () => {
             type='text'
             className={styles.codeInput}
             maxLength={1}
-            // onChange={(e) => console.log(inputsRef.current)}
+            onChange={e => handleNumber(e, index + 1)}
           />
         ))}
       </div>
