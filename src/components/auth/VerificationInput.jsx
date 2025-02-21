4181a64f95c10354223934f8cd117f4792e16797
soongu <naong526@naver.com>
2025-02-21 12:22:37 +0900

    # 34. 인증코드 유효시간 타이머

--- src/components/auth/VerificationInput.jsx
+++ src/components/auth/VerificationInput.jsx
@@ -12,6 +12,9 @@ const VerificationInput = ({ email }) => {
   // 에러 메시지
   const [error, setError] = useState('');
 
+  // 타이머 시간을 상태관리
+  const [timer, setTimer] = useState(300);
+
   // input태그들을 ref에 바인딩
   const bindInputRef = ($input, index) => {
     inputsRef.current[index] = $input;
@@ -38,6 +41,8 @@ const VerificationInput = ({ email }) => {
 
     // 검증에 실패했을 때
     if (!isMatch) {
+      // 타이머 리셋
+      setTimer(300);
       // 에러 메시지 세팅
       setError('유효하지 않거나 만료된 인증코드입니다. 인증코드를 재발송합니다.');
       // 인증코드 모두 지우기
@@ -95,10 +100,19 @@ const VerificationInput = ({ email }) => {
   //   }
   // }, [codes]);
 
-  // 초기 렌더링시 첫번째 input에 포커싱
+  // 초기 렌더링시 타이머 시작 및 첫번째 input에 포커싱
   useEffect(() => {
+
+    // 타이머 설정 - 1초마다 타이머시간을 리렌더링
+    const id = setInterval(() => { 
+      setTimer(prev => prev > 0 ? prev - 1 : 0);
+    }, 1000);
+
+
     inputsRef.current[0].focus();
     // console.log(codes.join(''));
+
+    return () => clearInterval(id); 
   }, []);
 
   return (
@@ -117,7 +131,10 @@ const VerificationInput = ({ email }) => {
           />
         ))}
       </div>
-      {error && <p className={styles.errorMessage}>{ error }</p> }
+      <div className={styles.timer}>
+        {`${'0' + Math.floor(timer / 60)}:${('0' + (timer % 60)).slice(-2)}`}
+      </div>
+      {error && <p className={styles.errorMessage}>{error}</p>}
     </>
   );
 };
